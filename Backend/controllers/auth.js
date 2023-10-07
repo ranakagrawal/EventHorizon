fee13e608b35cd3521522a145216fa887e1618e6
Murtaza <murtazabohraa@gmail.com>
2023-10-07 15:45:38 +0530

    userlogin function created in auth controller

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,1 +1,30 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+
+exports.userLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+        .then((result) => {
+        if (!result) {
+            res.status(404).json({ message: "No such user found!" });
+        } else {
+            bcrypt
+            .compare(password, result.password)
+            .then((isEqual) => {
+                if (!isEqual) {
+                res.status(404).json({ message: "Invalid password or email!" });
+                } else {
+                res.status(200).json({ message: "Login successful!" });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+        })
+        .catch((err) => {
+        console.log(err);
+        });
+};
