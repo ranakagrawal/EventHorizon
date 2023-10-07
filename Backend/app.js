613fad834abd4c0b0129fbf2ec58297b47a76e8e
Namit-Prajapati <namitprajapati51@gmail.com>
2023-10-07 17:31:29 +0530

    environment variable db

--- Backend/app.js
+++ Backend/app.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose'); 
-const bodyParser = require('body-parser'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const routes = require('./routes'); 
+const routes = require("./routes");
 
-app.use('/', routes); 
+app.use("/", routes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+  res.status(500).json({ message: "Something went wrong!" });
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://ranakagrawal:ranak123@college-work.j27dolr.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Database and server connected!");
     app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-      });
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch((err) => {
     console.log(err);
