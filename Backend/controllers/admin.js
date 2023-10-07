95d292b952b3023307c7340f776a2d6481dc9c14
Murtaza <murtazabohraa@gmail.com>
2023-10-07 16:34:52 +0530

    create venue function added

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -1,8 +1,25 @@
 const User = require("../models/user");
 const Event = require("../models/event");
 const Club = require("../models/club");
+const Venue = require("../models/venue");
 
 
-exports.addVenue = (req, res, next) => {
-    const { name, description, capacity } = req.body;
+exports.createVenue = async (req, res, next) => {
+    try{
+    const { name, description, capacity, venueImages} = req.body;
+
+    const newVenue = new Venue({
+        name,
+        venueImages,
+        description,
+        capacity,
+    });
+
+    await newVenue.save();
+
+    res.status(201).json({ message: 'Venue created successfully', venue: newVenue });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while creating the venue' });
+  }
 };
