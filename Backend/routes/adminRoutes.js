e95c810b123ec2c2a4a5e66b603505d5da830b08
Techwarium <techwariumdev@gmail.com>
2023-10-09 01:46:39 +0530

    creating user and club routes

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -9,6 +9,16 @@ const uploadVenueImages = multer({
   fileFilter: multerFunctions.imageFileFilter,
 }).array("image");
 
+const uploadStudentExcel = multer({
+  storage: multerFunctions.studentExcelStorage,
+  fileFilter: multerFunctions.xlsxFilter,
+}).single("studentexcel");
+
+const uploadFacultyExcel = multer({
+  storage: multerFunctions.facultyExcelStorage,
+  fileFilter: multerFunctions.xlsxFilter,
+}).single("facultyexcel");
+
 const adminController = require("../controllers/admin");
 ///////////////////////////////////////////
 //////// **Academic Event CRUD** //////////
@@ -20,10 +30,16 @@ router.post("/addacadevent", adminController.createAcademicEvent);
 router.post("/editacadevent/:id", adminController.editAcademicEvent);
 
 // DELETE route for deleting acad event by ID
-router.delete("/deleteacadevent/:id", adminController.deleteAcademicEvent);
+router.post("/deleteacadevent/:id", adminController.deleteAcademicEvent);
 
 // GET route for fetching acad events using ID
 router.get("/getacadevent/:id", adminController.getAcademicEventById);
+
+// GET route for fetching acad events happening on selected date
+router.get("/acadeventcurrdate/:date", adminController.getAcademicEventsOnCurrDate);
+
+// GET route for fetching acad events happening on or after selected date
+router.get("/acadeventafterdate/:date", adminController.getAcademicEventsAfterDate);
 
 // GET route for getting all of the academic events
 router.get("/getallacadevent", adminController.getAllAcademicEvent);
@@ -50,7 +66,7 @@ router.post(
 router.post("/editvenue/:id", adminController.editVenue);
 
 // DELETE route for deleting venue by ID
-router.delete("/deletevenue/:id", adminController.deleteVenue);
+router.post("/deletevenue/:id", adminController.deleteVenue);
 
 // GET route for fetching acad events using ID
 router.get("/getvenue/:id", adminController.getVenueById);
@@ -59,3 +75,47 @@ router.get("/getvenue/:id", adminController.getVenueById);
 router.get("/getallvenue", adminController.getAllVenue);
 
 module.exports = router;
+
+
+///////////////////////////////////////////
+////////// ** Creating User ** ////////////
+///l//////0///////r///////|)/////////M/////
+
+// Creating multiple Students from excel file with pass 'acro123'
+// excel file should have only one worksheet and dept names should be from schema
+router.post("/studentexcel", uploadStudentExcel, adminController.createStudentsFromExcel);
+
+
+// Creating multiple Faculty from excel file with pass 'acrofaculty123'
+// excel file should have only one worksheet and dept names should be from schema
+router.post("/facultyexcel", uploadFacultyExcel, adminController.createFacultyFromExcel);
+
+// Creating single users
+router.post("/createuser", adminController.createUser);
+
+///////////////////////////////////////////
+//////////// ***Club CRUD*** //////////////
+///l//////0///////r///////|)/////////M/////
+
+// POST route for creating a Club
+router.post("/createclub", adminController.createClub);
+
+// POST route for editing club name by ID
+router.post("/editclub/:id", adminController.editClubName);
+
+// POST route for adding faculty in club using ID
+//ek time pr ek hi
+router.post("/addfacultyclub/:id", adminController.addFacultyInClub);
+
+// DELETE route for deleting faculty from club using facultyID
+//ek time pr ek hi
+router.post("/deletefacultyclub/:id", adminController.deleteFacultyFromClub);
+
+// DELETE route for deleting entire club using clubId
+router.post("/deleteclub/:id", adminController.deleteClub);
+
+// GET route for fetching acad events using ID
+router.get("/getclubbyid/:id", adminController.getClubById);
+
+// GET route for getting all of the clubs
+router.get("/getallclub", adminController.getAllClub);
