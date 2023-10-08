456d7f60956a846cb1d8be048d7dbf7551fda588
Murtaza <murtazabohraa@gmail.com>
2023-10-09 00:05:43 +0530

    Club crud completed

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -451,118 +451,180 @@ exports.createUser = async (req, res, next) => {
 
 // POST route for creating a Club
 exports.createClub = async (req, res, next) => {
-  try {
-    const { name, facultyEmail } = req.body;
-
-    const faculty = await User.findOne({ email: facultyEmail });
-
-    if (!faculty) {
-      return res.status(404).json({ error: "Faculty not found" });
-    }
-
-    const newClub = new Club({
-      name,
-      facultyId: [faculty._id],
-    });
-
-    await newClub.save();
-
-    res.status(201).json({ message: "Club created successfully by Admin" });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while creating the Club" });
-  }
-};
-
-// POST route for editing acad event by ID
-exports.editAcademicEvent = async (req, res, next) => {
-  try {
-    const academicEventId = req.params.id; // Get acad event ID from request param
-    const { name, startDate, endDate, targetedDept } = req.body;
-
-    const targetedDeptArray = targetedDept.split(",");
-
-    const updatedAcademicEvent = await AcademicEvent.findByIdAndUpdate(
-      academicEventId,
-      {
+    try {
+      const { name, facultyEmail } = req.body;
+  
+      const faculty = await User.findOne({ email: facultyEmail });
+  
+      if (!faculty) {
+        return res.status(404).json({ error: "Faculty not found" });
+      }
+  
+      const newClub = new Club({
         name,
-        startDate,
-        endDate,
-        targetedDept: targetedDeptArray,
-      },
-      { new: true }
-    );
-
-    if (!updatedAcademicEvent) {
-      return res.status(404).json({ error: "Academic event not found" });
+        facultyId: [faculty._id],
+      });
+  
+      await newClub.save();
+  
+      res.status(201).json({ message: "Club created successfully by Admin" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while creating the Club" });
     }
-
-    res.status(200).json({
-      message: "Academic event updated successfully",
-      academicEvent: updatedAcademicEvent,
-    });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while updating the academic event" });
-  }
 };
-
-// DELETE route for deleting acad event by ID
-exports.deleteAcademicEvent = async (req, res, next) => {
-  try {
-    const academicEventId = req.params.id; // Get acad event ID from request param
-
-    const deletedAcademicEvent = await AcademicEvent.findByIdAndDelete(
-      academicEventId
-    );
-
-    if (!deletedAcademicEvent) {
-      return res.status(404).json({ error: "Academic event not found" });
+  
+// POST route for editing club name by ID
+exports.editClubName = async (req, res, next) => {
+    try {
+      const clubId = req.params.id; // Get club ID from request param
+      const { name } = req.body;
+  
+      const updatedClub = await Club.findByIdAndUpdate(
+        clubId,
+        {
+          name,
+        },
+        { new: true }
+      );
+  
+      if (!updatedClub) {
+        return res.status(404).json({ error: "Club not found" });
+      }
+  
+      res.status(200).json({
+        message: "Club name updated successfully",
+        club: updatedClub,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while updating the club name" });
     }
-
-    res.status(200).json({ message: "Academic event deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while deleting the academic event" });
-  }
 };
-
+  
+// POST route for adding faculty in club using ID
+//ek time pr ek hi
+exports.addFacultyInClub = async (req, res, next) => {
+    try {
+      const clubId = req.params.id; // Get club ID from request param
+      const { facultyEmail } = req.body;
+  
+      const faculty = await User.findOne({ email: facultyEmail });
+  
+      if (!faculty) {
+        return res.status(404).json({ error: "Faculty not found" });
+      }
+  
+      const updatedClub = await Club.findById(clubId);
+  
+      if (!updatedClub) {
+        return res.status(404).json({ error: "Club not found" });
+      }
+  
+      updatedClub.facultyId.push(faculty._id);
+      await updatedClub.save();
+  
+      res.status(200).json({
+        message: "Successfully added new Faculty member to the Club",
+        club: updatedClub,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({
+          error: "An error occurred while adding new faculty member to the club!",
+        });
+    }
+};
+  
+// DELETE route for deleting faculty from club using facultyID
+//ek time pr ek hi
+exports.deleteFacultyFromClub = async (req, res, next) => {
+    try {
+      const clubId = req.params.id; // Get club ID from request param
+      const facultyid = req.body;
+      const updatedFacultyId = [];
+  
+      const updatedClub = await Club.findById(clubId);
+  
+      if (!updatedClub) {
+        return res.status(404).json({ error: "Club not found" });
+      }
+  
+      updatedFacultyId = updatedClub.facultyId;
+      updatedFacultyId = updatedFacultyId.filter((path) => {
+        return path !== facultyid;
+      });
+  
+      updatedClub.facultyId = updatedFacultyId;
+      await updatedClub.save();
+  
+      res.status(200).json({ message: "faculty deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({
+          error: "An error occurred while deleting the faculty from club",
+        });
+    }
+};
+  
+// DELETE route for deleting entire club using clubId
+exports.deleteClub = async (req, res, next) => {
+    try {
+      const clubId = req.params.id; // Get club ID from request param
+  
+      const deletedClub = await Club.findByIdAndDelete(clubId);
+  
+      if (!deletedClub) {
+        return res.status(404).json({ error: "Club not found" });
+      }
+  
+      res.status(200).json({ message: "Club deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while deleting the Club" });
+    }
+};
+  
 // GET route for fetching acad events using ID
-exports.getAcademicEventById = async (req, res, next) => {
-  try {
-    const academicEventId = req.params.id; //get academic event ID from request param
-
-    const academicEvent = await AcademicEvent.findById(academicEventId);
-
-    if (!academicEvent) {
-      return res.status(404).json({ error: "Academic event not found" });
+exports.getClubById = async (req, res, next) => {
+    try {
+      const clubId = req.params.id; //get club ID from request param
+  
+      const club = await Club.findById(clubId);
+  
+      if (!club) {
+        return res.status(404).json({ error: "Club not found" });
+      }
+  
+      res.status(200).json({ club });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while retrieving the Club" });
     }
-
-    res.status(200).json({ academicEvent });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while retrieving the academic event" });
-  }
 };
-
-// GET route for getting all of the academic events
-exports.getAllAcademicEvent = async (req, res, next) => {
-  try {
-    const academicEvents = await AcademicEvent.find();
-
-    res.status(200).json({ academicEvents });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "An error occurred while retrieving all academic events",
-    });
-  }
+  
+// GET route for getting all of the clubs
+exports.getAllClub = async (req, res, next) => {
+    try {
+      const clubs = await Club.find();
+  
+      res.status(200).json({ clubs });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: "An error occurred while retrieving all Clubs",
+      });
+    }
 };
