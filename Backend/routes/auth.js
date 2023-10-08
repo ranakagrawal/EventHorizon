2d8a15ddb7f05ff01e1fcdd37227163caefc4c47
Techwarium <techwariumdev@gmail.com>
2023-10-08 12:46:41 +0530

    auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const router=express.Router()
+
+
 const authController=require("../controllers/auth")
+const isAuth = require("../middleware/is-auth");
 
 router.post("/login", authController.login);
 
