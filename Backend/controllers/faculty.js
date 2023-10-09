6b2a38820ed5d2b904c15a3dc14aacca7d9754f7
Namit-Prajapati <namitprajapati51@gmail.com>
2023-10-09 22:19:20 +0530

    faculty controllers part1

--- Backend/controllers/faculty.js
+++ Backend/controllers/faculty.js
@@ -9,3 +9,121 @@ const StudentAccess = require("../models/studentAccess");
 // edit access
 // club i am part of 
 // events organized by my club
+
+const User = require("../models/user");
+const Event = require("../models/event");
+const Club = require("../models/club");
+const Venue = require("../models/venue");
+const StudentAccess = require("../models/studentAccess");
+const { access } = require("fs");
+
+// provide student access
+// get students who have access
+// edit access
+// club i am part of
+// events organized by my club
+
+exports.createStudentAccess = async (req, res, next) => {
+  try {
+    const { studentEmail, facultyId, eventId } = req.body;
+    const student = await User.findOne({ email: studentEmail });
+    if (!student) {
+      return res
+        .status(403)
+        .json({ message: "No student with this email found!" });
+    }
+    if (student.role !== "student") {
+      return res
+        .status(403)
+        .json({ message: "Please enter a valid student email" });
+    }
+    const newStudentAccess = await new StudentAccess({
+      studentId: student._id,
+      facultyId,
+      eventId,
+      editable: true,
+    });
+
+    await newStudentAccess.save();
+
+    res.status(200).json({
+      message: `New Student Access created for ${student.name}`,
+      newStudentAccess,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error!" });
+  }
+};
+
+exports.getStudentAccess = async (req, res, next) => {
+  try {
+    const { eventId } = req.body;
+    const studentAccess = await StudentAccess.find({ eventId: eventId });
+    if (studentAccess.length === 0) {
+      return res
+        .status(203)
+        .json({ message: "Event does not have any student access" });
+    }
+
+    res.status(200).json({
+      studentAccess,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error!" });
+  }
+};
+
+exports.editStudentAccess = async (req, res, next) => {
+  try {
+    const { studentAccessId } = req.body;
+    const studentAcc = await StudentAccess.findById(studentAccessId);
+    if (!studentAcc) {
+      return res
+        .status(403)
+        .json({ message: "No student access with the Id found" });
+    }
+
+    studentAcc.editable = !studentAcc.editable;
+
+    await studentAcc.save();
+
+    res.status(200).json({
+      message: `Student Access updated`,
+      studentAcc,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error!" });
+  }
+};
+
+exports.getFacultyClubs = async (req, res, next) => {
+    try {
+      const {facultyId} = req.body;
+      const clubs = await Club.find({ facultyId: studentEmail });
+      if (!student) {
+        return res
+          .status(403)
+          .json({ message: "No student with this email found!" });
+      }
+      if (student.role !== "student") {
+        return res
+          .status(403)
+          .json({ message: "Please enter a valid student email" });
+      }
+      const newStudentAccess = await new StudentAccess({
+        studentId: student._id,
+        facultyId,
+        eventId,
+        editable: true,
+      });
+  
+      await newStudentAccess.save();
+  
+      res.status(200).json({
+        message: `New Student Access created for ${student.name}`,
+        newStudentAccess,
+      });
+    } catch (err) {
+      res.status(500).json({ message: "Server error!" });
+    }
+  };
