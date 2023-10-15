61eb3529360cf683ad2964355c873b97cda85690
Murtaza <murtazabohraa@gmail.com>
2023-10-15 22:47:11 +0530

    ranak ki mehnat

--- Backend/routes/qrRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-
-const multerFunctions = require("../middleware/multer");
-router.post("/createuser", adminController.createUser);
--- Backend/routes/studentRoutes.js
+++ Backend/routes/studentRoutes.js
@@ -7,23 +7,18 @@ const academicEventsController = require("../controllers/academicEvents");
 //////// **GET Academic Events** //////////
 ///l//////0///////r///////|)/////////M/////
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 // GET route for fetching acad events using ID
-router.get("/getacadevent/:id", academicEventsController.getAcademicEventById);
+router.get("/getacadevent/:id", studentController.getAcademicEventById);
 
 // GET route for fetching acad events happening on selected date
-router.get("/acadeventcurrdate/:date", academicEventsController.getAcademicEventsOnCurrDate);
+router.get("/acadeventcurrdate/:date", studentController.getAcademicEventsOnCurrDate);
 
 // GET route for fetching acad events happening on or after selected date
-router.get("/acadeventafterdate/:date", academicEventsController.getAcademicEventsAfterDate);
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// GET route for getting all of the academic events
-// router.get("/getallacadevent", academicEventsController.getAllAcademicEvent);
+router.get("/acadeventafterdate/:date", studentController.getAcademicEventsAfterDate);
 
-// GET route to get all of the academic events targeted to a particular department
-router.get(
-    "/academiceventbydept/:department",
-    academicEventsController.getAcademicEventByDept
-);
+// GET route for getting all of the academic events
+router.get("/getallacadevent", studentController.getAllAcademicEvent);
+
 
 module.exports=router
